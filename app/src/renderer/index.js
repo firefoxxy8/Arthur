f152b0e1f205685d10913ab687a00746d2e7037c
Steffen Bruchmann <hi@sbruchmann.me>
2017-09-20 20:41:11 +0200

    Setup basic ProseMirror editor

--- app/src/renderer/index.js
+++ app/src/renderer/index.js
@@ -1,4 +1,20 @@
-const $host = document.createElement("pre");
-$host.textContent = JSON.stringify(process.versions, null, 2);
+import {baseKeymap} from "prosemirror-commands";
+import {EditorState} from "prosemirror-state";
+import {EditorView} from "prosemirror-view";
+import {history, redo, undo} from "prosemirror-history";
+import {keymap} from "prosemirror-keymap";
+import {schema} from "prosemirror-schema-basic";
 
+const $host = document.createElement("div");
 document.body.appendChild($host);
+
+const state = EditorState.create({
+	schema,
+	plugins: [
+		history(),
+		keymap({"Mod-z": undo, "Mod-y": redo}),
+		keymap(baseKeymap),
+	],
+});
+
+const view = new EditorView($host, {state});
